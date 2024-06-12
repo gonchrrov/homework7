3bf48cac291238b486619ae7d263a77222b92822
Goncharov Anton <gonchrrv@icloud.com>
2024-06-12 20:21:13 +0300

    upd

--- script_acrchive.py
+++ script_acrchive.py
@@ -2,8 +2,6 @@ from zipfile import ZipFile
 
 with ZipFile('tmp/Hello World.zip') as zip_file:
     print(zip_file.namelist())
-
     text = zip_file.read('Hello.txt')
     print(text)
-
     zip_file.extract('Hello.txt', 'tmp')
