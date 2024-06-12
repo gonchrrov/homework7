74e82e33eaf07453a3572f0abfd2353328c3c60c
Goncharov Anton <gonchrrv@icloud.com>
2024-06-12 19:46:09 +0300

    upd script

--- script_os.py
+++ script_os.py
@@ -1,7 +1,16 @@
 import os.path
+import shutil
 
 CURRENT_FILE = (os.path.abspath(__file__))  # /Users/goncharov/QAGURU/homework7/script_os.py
 CURRENT_DIR = os.path.dirname(CURRENT_FILE)  # /Users/goncharov/QAGURU/homework7
 
 TMP_DIR = os.path.join(CURRENT_DIR, 'tmp')
 print(TMP_DIR)  # /Users/goncharov/QAGURU/homework7/tmp
+
+if not os.path.exists('tmp2'):
+    os.mkdir('tmp2')
+    print('Создал tmp2')
+else:
+    print('Не создал, уже есть.')
+
+shutil.rmtree(os.path.join(CURRENT_DIR, 'tmp2'))
